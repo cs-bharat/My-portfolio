a0be21fcc1e82fc874957e293478387d38fd413b
Bharat kumar <bharatkumarm413@gmail.com>
2024-05-18 21:02:03 +0530

    portfolio website ongoing

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-center underline text-3xl'>Hello bharat ready to build portfolio website</h1>
+    </>
+  )
+}
+
+export default App
